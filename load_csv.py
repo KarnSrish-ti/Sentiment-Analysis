3fac73d4a1bbdabf93ad9372728aa65c3ceafafb
anushka <mallikanushka80@gmail.com>
2025-07-21 23:17:52 +0545

    cleaning

--- load_csv.py
+++ load_csv.py
@@ -1,10 +1,9 @@
 import pandas as pd
+import re
+import os
+#changed path so that it is same for all
+file_path = os.path.join('dataset.csv')  # if dataset is in the same folder
 
-#Configuration
-#r before the string makes it a raw string so that backslashes (\) don’t break the path accidentally (like \n or \t being treated as special characters).
-
-
-file_path = r'D:\2nd_sem\Project2\Sentiment-Analysis\dataset.csv'
 
 #Main Logic
 def load_dataset(file_path, preview_rows=5):
@@ -23,7 +22,7 @@ def load_dataset(file_path, preview_rows=5):
         return safe_copy
 
     except FileNotFoundError:
-        print(f"File not found at: {path}")
+        print(f"File not found at: {file_path}")
         return None
     except Exception as e:
         print(f" Unexpected error: {e}")
@@ -31,3 +30,32 @@ def load_dataset(file_path, preview_rows=5):
 
 # Run
 df = load_dataset(file_path, preview_rows=5)
+
+# Rename the columns as specified
+df.columns = ['Target', 'Predicted Label', 'Remarks', 'Sentences']
+
+# Define a function to clean text
+def clean_text(text):
+    # Remove URLs
+    text = re.sub(r'http\S+|www\S+', '', str(text))
+    # Remove special characters, numbers, punctuation, and Nepali full stop (keeping only Devanagari characters and spaces)
+    text = re.sub(r'[^ऀ-ॿ\s]', '', text)
+    # Remove Nepali numbers (०-९)
+    text = re.sub(r'[०१२३४५६७८९]', '', text)
+    # Strip Nepali full stop
+    text = text.replace('।', '')
+    # Remove extra spaces
+    text = re.sub(r'\s+', ' ', text).strip()
+    #remove repeate characters
+    text = re.sub(r'(.)\1{2,}', r'\1', text)  
+    return text
+
+
+# Apply cleaning function only to the 'Sentences' column
+df['Sentences'] = df['Sentences'].apply(clean_text)
+
+# Remove duplicate sentences
+df = df.drop_duplicates(subset=['Sentences']).reset_index(drop=True)
+
+#save the new dataset
+df.to_csv('cleaned_dataset.csv', index=False)
